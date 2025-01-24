ca1a9ded722482ebe1897e64f8d1c983daef26c0
Igor Razvodovsky <igors.razvodovskis@tacton.com>
2025-01-24 16:05:48 +0100

    Add theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -10,7 +10,7 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
-    },
+    }
   },
 };
 
---
+++ .storybook/theme.js
@@ -0,0 +1,20 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  fontBase: 'system-ui, sans-serif',
+  fontCode: 'monospace',
+  brandTitle: 'Igor\'s design system playground',
+  brandImage: '../src/assets/slide.png',
+  brandTarget: '_self',
+
+  colorPrimary: 'red',
+  colorSecondary: 'gray',
+
+  // UI
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  appPreviewBg: '#ffffff',
+  appBorderColor: 'rgba(38, 85, 115, 0.15)',
+  appBorderRadius: 4,
+});
