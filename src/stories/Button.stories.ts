31943bdec0d86c00ac184ec888e454f026dec03f
Igor Razvodovsky <igors.razvodovskis@tacton.com>
2025-01-24 15:13:51 +0100

    Add button styles

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
-import type { ButtonProps } from "../components/button/button";
 import { html } from "lit";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
@@ -15,10 +14,10 @@ const meta = {
     },
     loading: { control: 'boolean' }
   },
-} satisfies Meta<ButtonProps>;
+} satisfies Meta;
 
 export default meta;
-type Story = StoryObj<ButtonProps>;
+type Story = StoryObj;
 
 export const Default: Story = {
   args: {
@@ -26,7 +25,36 @@ export const Default: Story = {
     loading: false,
     size: "medium"
   },
-  render: (args) => html` <ir-button> <iconify-icon icon="ph:circle-dashed"></iconify-icon> ${args.label} </ir-button>`,
+  render: (args) => html` <button is="ir-button"> ${args.label} </button>`,
 };
 
+export const Variants: Story = {
+  render: () => html`
+    <button class="button--primary" is="ir-button">Primary</button>
+    <button class="button--danger" is="ir-button">Danger</button>
+  `,
+};
+
+// export const Sizes: Story = {
+//   render: () => html` <button is="ir-button">Medium</button>`,
+// };
+
+export const Prefixes: Story = {
+  render: () => html`
+    <button is="ir-button"><iconify-icon class="icon" icon="ph:circle-dashed"></iconify-icon> With prefix</button>
+    <button is="ir-button">With suffix <iconify-icon class="icon" icon="ph:circle-dashed"></iconify-icon></button>
+    <button is="ir-button"><iconify-icon class="icon" icon="ph:circle-dashed"></iconify-icon> With both <iconify-icon class="icon" icon="ph:circle-dashed"></iconify-icon></button>
+    `,
+};
+
+export const Toggle: Story = {
+  render: () => html`
+    <button is="ir-button">Simple toggle</button>
+    <button is="ir-button">Multiple state toggle</button>
+  `,
+};
+
+export const IconButton: Story = {
+  render: (args) => html` <button is="ir-button"> <iconify-icon class="icon" icon="ph:circle-dashed"></iconify-icon><span class="inclusively-hidden">${args.label}</span></button>`,
+};
 
