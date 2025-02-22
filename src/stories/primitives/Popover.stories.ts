94c4f57a8452344849c64f99533439ce721062ed
Igor Razvodovsky <igors.razvodovskis@tacton.com>
2025-02-22 09:32:31 +0100

    Popover: add another example

--- src/stories/primitives/Popover.stories.ts
+++ src/stories/primitives/Popover.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import { faker } from '@faker-js/faker';
 
 const meta = {
   title: "Primitives/Popover",
@@ -8,11 +9,46 @@ const meta = {
 export default meta;
 type Story = StoryObj;
 
-export const Default: Story = {
+export const Tooltip: Story = {
   render: () => html`
-    <button popovertarget="my-popover" style="anchor-name: --anchor_1;">Popover</button>
-    <div id="my-popover" popover>
+    <button popovertarget="popover-tooltip" style="anchor-name: --anchor_1">Hover me</button>
+    <div id="popover-tooltip" class="tooltip" popover>
       <p>I am a popover with more information. Hit <kbd>esc</kbd> or click away to close me.</p>
+    </div>
+
+    <script>
+      const button = document.querySelector('[popovertarget="popover-tooltip"]');
+      let timeout = 0;
+        button.addEventListener("mouseenter", () => {
+          const target = button.getAttribute("popovertarget");
+          const popover = document.querySelector("#" + target);
+          timeout = setTimeout(() => {
+            popover.showPopover();
+          }, 1000);
+        });
+
+        button.addEventListener("mouseleave", () => {
+          clearTimeout(timeout);
+        });
+    </script>
+  `,
+};
+
+export const Toast: Story = {
+  render: () => html`
+    <button popovertarget="popover-toast">Toast!</button>
+    <div id="popover-toast" class="toast" popover>
+      <p>I am a popover with more information. Hit <kbd>esc</kbd> or click away to close me.</p>
+    </div>
+  `,
+};
+
+export const Popover: Story = {
+  render: () => html`
+    <button popovertarget="popover-1" style="anchor-name: --anchor_2">Click me</button>
+    <div id="popover-1" popover>
+      <strong>Popover header</strong>
+      <p>${faker.hacker.phrase()}</p>
     </div>
   `,
 };
