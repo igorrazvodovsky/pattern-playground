82b1e8ae7f2fac4c505e4b15bbb8e123da77d912
Igor Razvodovsky <igors.razvodovskis@tacton.com>
2025-05-21 21:48:13 +0200

    Update filtering template

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -1,16 +1,16 @@
 export class PpAvatar extends HTMLElement {
-  private _size: 'small' | 'medium' | 'large' = 'medium';
+  private _size: 'xsmall' | 'small' | 'medium' | 'large' = 'medium';
 
   constructor() {
     super();
     this.classList.add('pp-avatar', 'pp-avatar__container', `pp-avatar--${this._size}`);
   }
 
-  get size(): 'small' | 'medium' | 'large' {
+  get size(): 'xsmall' | 'small' | 'medium' | 'large' {
     return this._size;
   }
 
-  set size(value: 'small' | 'medium' | 'large') {
+  set size(value: 'xsmall' | 'small' | 'medium' | 'large') {
     if (this._size !== value) {
       this.classList.remove(`pp-avatar--${this._size}`);
       this._size = value;
--- src/stories/compositions/Filtering.stories.ts
+++ src/stories/compositions/Filtering.stories.ts
@@ -11,35 +11,67 @@ type Story = StoryObj;
 export const Filtering: Story = {
   args: {},
   render: () => html`
-    <div class="tags">
-      <div class="tag-group">
-        <span class="tag">
-          Status
-        </span>
-        <span class="tag">
-          is
-        </span>
-        <span class="tag">
-          Todo
-          <button>
-            <iconify-icon class="icon" icon="ph:x"></iconify-icon>
-          </button>
-        </span>
-      </div>
+    <div class="flex">
+      <div class="tags">
+        <div class="tag-group">
+          <span class="tag">
+            Status
+          </span>
+          <pp-dropdown>
+            <button class="tag" slot="trigger">
+              is
+            </button>
+            <pp-list>
+              <pp-list-item type="checkbox" checked>is</pp-list-item>
+              <pp-list-item>is not</pp-list-item>
+            </pp-list>
+          </pp-dropdown>
+          <span class="tag">
+            <pp-dropdown>
+              <button slot="trigger">
+                <div class="avatar-group">
+                  <pp-avatar size="xsmall" style="color: blue">
+                    <iconify-icon class="icon" icon="ph:circle-fill"></iconify-icon>
+                  </pp-avatar>
+                  <pp-avatar size="xsmall" style="color: green">
+                    <iconify-icon class="icon" icon="ph:circle-fill"></iconify-icon>
+                  </pp-avatar>
+                </div>
+                2 selected
+              </button>
+              <pp-list>
+                <pp-list-item type="checkbox" checked>Todo</pp-list-item>
+                <pp-list-item>...</pp-list-item>
+              </pp-list>
+            </pp-dropdown>
+            <button class="tag-group__remove">
+              <iconify-icon class="icon" icon="ph:x"></iconify-icon><span class="inclusively-hidden">Clear</span>
+            </button>
+          </span>
+        </div>
 
-      <div class="tag-group">
-        <span class="tag">
-          Due date
-        </span>
-        <span class="tag">
-          before
-        </span>
-        <span class="tag">
-          3 days from now
-          <button>
-            <iconify-icon class="icon" icon="ph:x"></iconify-icon>
-          </button>
-        </span>
+        <div class="tag-group">
+          <span class="tag">
+            Due date
+          </span>
+          <span class="tag">
+            before
+          </span>
+          <span class="tag">
+            3 days from now
+            <button class="tag-group__remove">
+              <iconify-icon class="icon" icon="ph:x"></iconify-icon><span class="inclusively-hidden">Clear</span>
+            </button>
+          </span>
+        </div>
+      </div>
+      <div class="button-group">
+        <button class="button" is="pp-buton">
+          <iconify-icon class="icon" icon="ph:x"></iconify-icon><span class="inclusively-hidden">Clear all</span>
+        </button>
+        <button class="button" is="pp-buton">
+          <iconify-icon class="icon" icon="ph:funnel-simple"></iconify-icon><span class="inclusively-hidden">Filter</span>
+        </button>
       </div>
     </div>
   `,
