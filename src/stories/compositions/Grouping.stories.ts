690fe0438a7faae4af097e31b65905034fd6fd2a
Igor Razvodovsky <igors.razvodovskis@tacton.com>
2025-05-22 12:34:00 +0200

    Grouping: add a button

--- src/stories/compositions/Grouping.stories.ts
+++ src/stories/compositions/Grouping.stories.ts
@@ -10,6 +10,18 @@ const meta = {
 export default meta;
 type Story = StoryObj;
 
+type CardData = {
+  img: string;
+  title: string;
+  description: string;
+  date: string;
+}
+
+type GroupData = {
+  name: string;
+  cards: CardData[];
+}
+
 const createCardData = () => {
   return {
     img: faker.image.url(),
@@ -19,20 +31,29 @@ const createCardData = () => {
   }
 };
 
-const cardsData = Array.from({ length: 3 }, () =>
-  faker.helpers.multiple(createCardData, { count: faker.number.int({ min: 3, max: 6 }) })
-)
+const cardsData: GroupData[] = Array.from({ length: 3 }, () => ({
+  name: "",
+  cards: faker.helpers.multiple(createCardData, { count: faker.number.int({ min: 3, max: 6 }) })
+}))
+
+cardsData[0].name = "Small";
+cardsData[1].name = "Medium";
+cardsData[2].name = "Large";
 
 export const Grouping: Story = {
   args: {},
   render: () => html`
     <div class="flow layer">
+      <button class="button" is="pp-button">
+        <iconify-icon class="icon" icon="ph:stack"></iconify-icon>
+        Grouped by <strong>size</strong>
+      </button>
       ${repeat(cardsData, (group, index) => html`
         <div>
           <details class="layer borderless" ?open=${index !== 1}>
-            <summary>Group ${String.fromCharCode(65 + index)} <span class="badge">${group.length}</span></summary>
+            <summary>${group.name} <span class="badge">${group.cards.length}</span></summary>
             <ul class="cards layout-grid">
-              ${repeat(group, (card) => html`
+              ${repeat(group.cards, (card) => html`
                 <li>
                   <article class="card layer">
                     <a class="label" href="#">${card.title}</a>
