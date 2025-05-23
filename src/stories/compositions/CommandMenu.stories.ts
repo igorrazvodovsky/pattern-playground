9587d641a46c40493f04cb9625de1e3348aa283f
Igor Razvodovsky <igors.razvodovskis@tacton.com>
2025-05-23 19:29:40 +0200

    Describe Command Menu

---
+++ src/stories/compositions/CommandMenu.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+const meta = {
+  title: "Compositions/Command menu*",
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj;
+
+export const CommandMenu: Story = {
+  args: {},
+  render: () => html`
+
+  `,
+};
