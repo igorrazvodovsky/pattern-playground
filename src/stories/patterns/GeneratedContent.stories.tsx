56545d26ef54e9dc34821d56485ce550ac8528b3
Igor Razvodovsky <igorrazvodovsky@gmail.com>
2025-07-31 10:47:21 +0200

    Move 'generated content'

--- src/stories/patterns/GeneratedContent.stories.tsx
+++ src/stories/patterns/GeneratedContent.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 const meta = {
-  title: "Patterns/Communication/Generated content",
+  title: "Patterns/Generated content",
 } satisfies Meta;
 
 export default meta;
